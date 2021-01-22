8a4f61acfc9634252fa85327d35b121b2c124f29
Amarta Dey <34670651+amartadey@users.noreply.github.com>
2021-01-23 00:47:30 +0530

    API Practice

--- Udemy Web API/script.js
+++ Udemy Web API/script.js
@@ -1,21 +1,30 @@
-const friend1 = {
-  name: "Laurence",
-};
-const friend2 = {
-  name: "John",
-};
-const friend3 = {
-  name: "Jane",
-};
-const friends = [friend1, friend2];
-friends.push(friend3);
+const url = "https://dummyapi.io/data/api/user?limit=10";
 
-friends.forEach((friend) => {
-  friend.lastname = "Smith";
-});
-console.log(JSON.stringify(friends));
-const str =
-  '[{"name":"Laurence","lastname":"Smith"},{"name":"John","lastname":"Smith"},{"name":"Jane","lastname":"Smith"}]';
-const parse = JSON.parse(str);
+// FETCH
+// fetch(url, { headers: { "app-id": "600ae2e27dff4c0c80425c7b" } })
+//   .then((res) => {
+//     return res.json();
+//   })
+//   .then((res) => console.log(res.data));
 
-console.log(parse);
+// XMLHTTPREQUEST
+// let xhr = new XMLHttpRequest();
+// xhr.open("GET", url);
+// xhr.setRequestHeader("app-id", "600ae2e27dff4c0c80425c7b");
+// xhr.responseType = "json";
+// xhr.onload = function () {
+//   console.log(xhr.response);
+// };
+
+// xhr.send();
+// console.log(xhr);
+
+fetch(url, { headers: { "app-id": "600ae2e27dff4c0c80425c7b" } })
+  .then((res) => res.json())
+  .then((datas) => {
+    const allData = datas.data;
+    console.log(allData);
+    allData.forEach((data) => {
+      console.log(data.email);
+    });
+  });
